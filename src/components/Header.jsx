9b08d6ae4305cb30d8814057d72e2d3f7bd49cf4
shm rsuf <shmusuf@gmail.com>
2024-01-25 20:44:45 +0600

    cartDetails created and showCart functionality added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Moon from "../assets/icons/moon.svg";
 import Logo from "../assets/logo.svg";
 import Ring from "../assets/ring.svg";
 import ShoppingCart from "../assets/shopping-cart.svg";
+import CartDetails from "../cine/CartDetails";
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  /* handleShowCart */
+  const handleShowCart = () => {
+    setShowCart(true);
+  };
+
   return (
     <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
       <nav className='container flex items-center justify-between space-x-10 py-6'>
         <a href='index.html'>
           <img src={Logo} width='139' height='26' alt='logo' />
@@ -31,6 +41,7 @@ const Header = () => {
             <a
               className='bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block'
               href='#'
+              onClick={handleShowCart}
             >
               <img
                 src={ShoppingCart}
