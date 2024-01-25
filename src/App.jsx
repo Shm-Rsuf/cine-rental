84c3b9835913686d11f72f06ebfde78aeb5f00da
shm rsuf <shmusuf@gmail.com>
2024-01-25 23:45:23 +0600

    context and provider added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import MovieList from "./cine/MovieList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
+import { MovieContext } from "./context";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <main>
-        <div className='container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]'>
-          <SideBar />
-          <MovieList />
-        </div>
-      </main>
-      <Footer />
+      <MovieContext.Provider>
+        <Header />
+        <main>
+          <div className='container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]'>
+            <SideBar />
+            <MovieList />
+          </div>
+        </main>
+        <Footer />
+      </MovieContext.Provider>
     </>
   );
 }
