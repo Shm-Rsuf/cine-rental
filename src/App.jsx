387be7314331e7b672ef1c1d0f6c501bd7d46287
shm rsuf <shmusuf@gmail.com>
2024-01-25 20:27:58 +0600

    cine rental initialize and 40% of this project done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import MovieList from "./cine/MovieList";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className='container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]'>
+          <SideBar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
